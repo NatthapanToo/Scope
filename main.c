08eb30f6cc59b3242b77ee26265b2705cc5fdaf3
Mingmanas Sivaraksa <62439174+AjMing@users.noreply.github.com>
2022-01-07 09:35:15 +0000

    submit

--- main.c
+++ main.c
@@ -1,66 +1,33 @@
-/*******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 
-void useLocal();
-void useStaticLocal();
-void useGlobal();
+int main(int argc,char*argv[]){
+  //atoi atof
 
-int x=1;  //global -->10
-
-int main()
-{
-   printf("Global x in main is %d \n",x);
-   int x=5;// local in main
+    int i,N;
+    int *a; // argv <- a
+    
+    
+    N=argc-1;
+    a=(int*) malloc (sizeof(int)*N);
+    //a[N]
    
-   printf("Local x in main is %d \n",x);
-  
-  
-   {  //block start new scope
-       int x=7;// hides both x in outer scope and global xor_eqconst_cast
-       printf("Local x in main's inner scope is %d \n",x);
-   }
-
-  printf("Local x in main is %d \n",x);
-  printf("\n========================\n");
-  useLocal();
-  useLocal();
+    for(i= 1;i<argc ;i++){
         
-  printf("\n========================\n");
-  useStaticLocal();
-  useStaticLocal();
-  printf("\n========================\n");
-  useGlobal();
-  useGlobal();
-         
-  return 0;
+        a[i-1]=atoi(argv[i]);
+       
+
+    }
+
+    int sum=0;
+  for(i= 0;i<N ;i++){
+       if(a[i]%2){
+
+        printf("%d\n",a[i]);
+         sum+=a[i];
+        }
+  }
+
+  printf("Sum of odd numbers is %d\n",sum);    
 }
 
-void useLocal(){
-    int x=25;
-    
-    printf("\nlocal x is %d on entering useLocal\n",x);
-    
-     ++x;   
-    printf("\nlocal x is %d on exiting useLocal\n",x);
-}
-
-
-void useStaticLocal(){
-   static int x=50;
-    
-    printf("\nlocal x is %d on entering useStaticLocal\n",x);
-    
-     ++x;   
-    printf("\nlocal x is %d on exiting useStaticLocal\n",x);
-
-}
-
-void useGlobal(){
-   
-    
-    printf("\nlocal x is %d on entering useGlobal\n",x);
-    
-    x*=10;   
-    printf("\nlocal x is %d on exiting useGlobal\n",x);
-
-}
